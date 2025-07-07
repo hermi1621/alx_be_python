2aa87401dbb8f6f136fd2672c1ff60c6d8824abd
hermi1621 <hermelabezabih4@gmail.com>
2025-07-07 10:30:30 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,17 +3,11 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_details(self):
-        return f"Book: {self.title} by {self.author}"
-
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
-
-    def get_details(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
@@ -21,20 +15,19 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-
 
 class Library:
     def __init__(self):
         self.books = []
 
     def add_book(self, book):
-        if isinstance(book, Book):
-            self.books.append(book)
-        else:
-            print("Only Book instances can be added.")
+        self.books.append(book)
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
+            if isinstance(book, EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
+            elif isinstance(book, PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"Book: {book.title} by {book.author}")
